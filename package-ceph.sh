1d208f4f746bf1ca8a1de3f2d05f5f6a3773a204
Tommi Virtanen <tv@inktank.com>
2012-08-17 17:16:17 -0700

    WIP

--- package-ceph.sh
+++ package-ceph.sh
@@ -1,122 +1,395 @@
-#!/bin/bash
+#!/usr/bin/python
+import argparse
+import errno
+import os
+import shutil
+import subprocess
+import sys
+import tempfile
+import yaml
+import hashlib
 
-#Package Ceph Barclamp
-#This script makes packaging the ceph barclamp simple (hopefully)
-#jj.galvez@inktank.com
 
-if [ ! -d "$1" ]; then
-	echo "Pass the name of the barclamp folder you are trying to package."
-	exit;
-fi
+def maybe_mkdir(*a, **kw):
+    try:
+        os.mkdir(*a, **kw)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            pass
+        else:
+            raise
 
-if [ -f "$1/crowbar.yml" ]; then
-	echo "Verified the crowbar.yml file exists."
-else
-	echo "No crowbar.yml file exists!"
-	exit;
-fi
 
-workingdir=$1
-name=`grep 'name: ' ${workingdir}/crowbar.yml | awk {'print \$2'}`
-startdir=`pwd`
+class Error(Exception):
+    pass
 
-tmpfolder="/tmp/package-barclamp"
-cachefolder="${tmpfolder}/cache"
-mirrorfolder="${tmpfolder}/mirrors"
-gemfolder="${workingdir}/cache/gems"
 
-statusfile="${tmpfolder}/status"
-sourcefile="${tmpfolder}/sources.list"
+def download(args):
+    """
+    Download packages.
+    """
 
-distrolist="ubuntu-12.04"
-packagelist="ceph ceph-dbg ceph-common ceph-common-dbg libcephfs1 gdisk"
-gemlist="open4"
+    packages = args.yaml['debs']['pkgs']
 
-echo "Destroying package cache folder"
-if [ -d "$workingdir/cache" ]; then
-	rm -rf ${workingdir}/cache
-fi
-echo "Done."
+    maybe_mkdir('cache')
+    maybe_mkdir('cache/tmp')
+    apt = 'cache/tmp/apt'
+    for path in [
+        apt,
+        os.path.join(apt, 'conf'),
+        os.path.join(apt, 'conf/preferences.d'),
+        os.path.join(apt, 'conf/sources.list.d'),
+        os.path.join(apt, 'conf/trusted.gpg.d'),
+        os.path.join(apt, 'cache'),
+        os.path.join(apt, 'state'),
+        ]:
+        maybe_mkdir(path)
 
-echo "Creating tmp folders..."
-mkdir -p $tmpfolder
-mkdir -p $cachefolder
-mkdir -p $mirrorfolder
-echo "Done."
-
-touch $sourcefile
-
-echo "Writing to tmp sources.list..."
-tee $sourcefile <<-'EOL'
+    with file(os.path.join(apt, 'conf/sources.list'), 'w') as f:
+        f.write("""\
 deb http://gitbuilder.ceph.com/ceph-deb-precise-x86_64-basic/ref/master precise main
 deb http://us-west-1.ec2.archive.ubuntu.com/ubuntu/ precise main restricted universe multiverse
 deb http://us-west-1.ec2.archive.ubuntu.com/ubuntu/ precise-updates main restricted universe multiverse
 deb http://us-west-1.ec2.archive.ubuntu.com/ubuntu/ precise-security main restricted universe multiverse
-EOL
-echo "Done."
+""")
 
-echo "Creating an empty status file..."
-cat /dev/null > ${statusfile}
-echo "Done."
+    # TODO somehow put a good /var/lib/dpkg/status mock state in here;
+    # avoid installing everything starting from libc
+    with file(os.path.join(apt, 'state/status'), 'w') as f:
+        pass
+#         for package in [
+#             'binutils',
+#             'hdparm',
+#             'libc6',
+#             'libedit2',
+#             'libgcc1',
+#             'libkeyutils1',
+#             'libnspr4',
+#             'libnss3',
+#             'libstdc++6',
+#             'libuuid1',
+#             'sdparm',
 
-echo "Updating aptitude with tmp sources.list"
-sudo apt-get -qq update -o=Dir::Cache::archives=${cachefolder} -o=Dir::State:status=${statusfile} -o=Dir::State=${tmpfolder} -o=Dir::Etc::sourcelist=${sourcefile}
-echo "Done."
+# # coreutils
+# # debconf
+# # debianutils
+# # dpkg
+# # gcc-4.6-base
+#             'groff-base',
+# # libacl1
+# # libaio1
+# # libattr1
+# # libbsd0
+# # libbz2-1.0
+# # libc-bin
+# # libc6
+# # libcrypto++9
+# # libdb5.1
+#             'libedit2',
+# # libexpat1
+#             'libgcc1',
+# # libgoogle-perftools0
+#             'libicu48',
+#             'libkeyutils1',
+# # liblzma5
+#             'libncurses5',
+# # libncursesw5
+# # libpam-modules
+# # libpam-modules-bin
+# # libpam0g
+#             'libpopt0',
+# # libreadline6
+# # libselinux1
+# # libsqlite3-0
+# # libssl1.0.0
+#             'libstdc++6',
+# # libtcmalloc-minimal0
+#             'libtinfo5',
+# # libunwind7
+#             'libuuid1',
+# # mime-support
+# # multiarch-support
+# # passwd
+# # perl-base
+# # python
+# # python-minimal
+# # python2.7
+# # python2.7-minimal
+# # readline-common
+# # sdparm
+# # sensible-utils
+# # tar
+# # tzdata
+# # uuid-runtime
+# # xz-utils
+# # zlib1g
 
-for distro in ${distrolist}; do
+#             ]:
+#             f.write("""\
+# Package: {p}
+# Status: install ok installed
 
-	distrocachedir="${workingdir}/cache/${distro}/pkgs"
-	mkdir -p ${distrocachedir}
+# """.format(p=package))
 
-	for package in ${packagelist}; do
-                echo "Grabbing dependencies for ${package}..."
-		for dep in `echo $(apt-cache show -o=Dir::Cache::archives=${cachefolder} -o=Dir::State:status=${statusfile} -o=Dir::State=${tmpfolder} -o=Dir::Etc::sourcelist=${sourcefile} ${package} | grep Depends | awk -F'Depends: ' {'print $2'} | sed 's/([^()]*)//g' | sed 's/,/ /g' | sed 's/|/ /g')`; do 
-			echo "Grabbing ${dep}..."
-			sudo apt-get -qq install -y -d --reinstall --allow-unauthenticated -o=Dir::Cache::archives=${cachefolder} -o=Dir::State:status=${statusfile} -o=Dir::State=${tmpfolder} -o=Dir::Etc::sourcelist=${sourcefile} ${dep}
-			echo "Done."
-		done
-		echo "Grabbing package ${package}"
-		sudo apt-get -qq install -y -d --reinstall --allow-unauthenticated -o=Dir::Cache::archives=${cachefolder} -o=Dir::State:status=${statusfile} -o=Dir::State=${tmpfolder} -o=Dir::Etc::sourcelist=${sourcefile} ${package}
-		echo "Done."
-	done
+    shutil.copyfile(
+        '/usr/share/apt/ubuntu-archive.gpg',
+        os.path.join(apt, 'conf/trusted.gpg.d/ubuntu-archive.gpg'),
+        )
 
-	echo "Moving all downloaded packages from ${cachefolder}/ to ${distrocachedir}/..."
-	sudo mv -f ${cachefolder}/*.deb ${distrocachedir}/
-	echo "Files have been moved."
+    with tempfile.TemporaryFile(
+        prefix='package-barclamp-',
+        suffix='.tmp',
+        ) as tmp:
+        # bleh easiest way to verify ssl certs with ubuntu 12.04 python is
+        # to shell out to wget
+        subprocess.check_call(
+            args=[
+                'wget',
+                '-nv',
+                '-O-',
+                'https://raw.github.com/ceph/ceph/master/keys/release.asc',
+                'https://raw.github.com/ceph/ceph/master/keys/autobuild.asc',
+                ],
+            stdout=tmp,
+            )
+        tmp.seek(0)
+        subprocess.check_call(
+            args=[
+                'gpg',
+                '--no-default-keyring',
+                '--keyring', os.path.join(apt, 'conf/trusted.gpg.d/ceph.gpg'),
+                '--import',
+                ],
+            stdin=tmp,
+            )
 
-	echo "Correcting permissions..."
-	sudo chown $USER:$(id -gn $USER) ${distrocachedir}/*.deb
-	echo "Permissions fixed."
+    abs_apt = os.path.abspath(apt)
+    subprocess.check_call(
+        args=[
+            'apt-get',
+            '-q=1',
+            '-o=Dir::Etc=' + os.path.join(abs_apt, 'conf'),
+            '-o=Dir::Cache=' + os.path.join(abs_apt, 'cache'),
+            '-o=Dir::State=' + os.path.join(abs_apt, 'state'),
+            '-o=Dir::State::status=' + os.path.join(abs_apt, 'state/status'),
+            'update',
+            ],
+        )
 
-	echo "Generating Packages.gz for ${distrocachedir}/"
-	cd ${distrocachedir}/
-	dpkg-scanpackages . /dev/null 2>/dev/null | gzip -9 > Packages.gz
-	echo "Done."
+#     subprocess.check_call(
+#         args=[
+#             'apt-get',
+#             '-q=1',
+#             '-y',
+#             '-o=Dir::Etc=' + os.path.join(abs_apt, 'conf'),
+#             '-o=Dir::Cache=' + os.path.join(abs_apt, 'cache'),
+#             '-o=Dir::State=' + os.path.join(abs_apt, 'state'),
+#             '-o=Dir::State::status=' + os.path.join(abs_apt, 'state/status'),
+#             'install',
+#             '--no-install-recommends',
+#             '-d',
+# #            '--reinstall',
+#             '--',
+#             ] + PACKAGE_LIST,
+#         cwd=
+#         )
+#     maybe_mkdir('cache')
+#     maybe_mkdir('cache/ubuntu-12.04')
+#     maybe_mkdir('cache/ubuntu-12.04/pkgs')
+#     for fn in os.listdir('cache/ubuntu-12.04/pkgs'):
+#         if not fn.endswith('.deb'):
+#             continue
+#         os.unlink(os.path.join('cache/ubuntu-12.04/pkgs', fn))
+#     for fn in os.listdir(os.path.join(apt, 'cache/archives')):
+#         if not fn.endswith('.deb'):
+#             continue
+#         os.link(
+#             os.path.join(apt, 'cache/archives', fn),
+#             os.path.join('cache/ubuntu-12.04/pkgs', fn),
+#             )
 
-done
+    maybe_mkdir('cache')
+    maybe_mkdir('cache/ubuntu-12.04')
+    maybe_mkdir('cache/ubuntu-12.04/pkgs')
+    subprocess.check_call(
+        args=[
+            'apt-get',
+            '-q=1',
+            '-y',
+            '-o=Dir::Etc=' + os.path.join(abs_apt, 'conf'),
+            '-o=Dir::Cache=' + os.path.join(abs_apt, 'cache'),
+            '-o=Dir::State=' + os.path.join(abs_apt, 'state'),
+            '-o=Dir::State::status=' + os.path.join(abs_apt, 'state/status'),
+            'download',
+            '--',
+            ] + packages,
+        cwd='cache/ubuntu-12.04/pkgs',
+        )
 
-cd ${workingdir}
+    subprocess.check_call(
+        args=[
+            'dpkg-scanpackages',
+            '.',
+            '/dev/null',
+            ],
+        cwd='cache/ubuntu-12.04',
+        stdout=file(os.path.join('cache/ubuntu-12.04/pkgs/Packages'), 'wb'),
+        stderr=file('/dev/null', 'wb'),
+        )
+    subprocess.check_call(
+        args=[
+            'gzip',
+            '-9',
+            '-f',
+            'Packages',
+            ],
+        cwd='cache/ubuntu-12.04/pkgs',
+        )
 
-echo "Making folder for gems."
-mkdir -p ${gemfolder}
-cd ${gemfolder}
-echo "Done."
 
-echo "Downloading Gems..."
-for gem in ${gemlist}; do
-	gem fetch ${gem}
-done
-echo "Done."
+def sha1(args):
+    """
+    Generate the sha1sum file.
+    """
+    def reraise(e):
+        raise e
+    with file('sha1sums', 'w') as sums:
+        for dirpath, dirnames, filenames in os.walk('.', onerror=reraise):
+            dirnames.sort()
+            filenames.sort()
 
-echo "Removing tmp folder..."
-sudo rm -rf $tmpfolder
-echo "Done."
+            if dirpath == '.':
+                try:
+                    filenames.remove('sha1sums')
+                except ValueError:
+                    pass
 
-echo "Setting aptitude back to normal."
-sudo apt-get -qq update
-echo "Done."
+            try:
+                dirnames.remove('.git')
+            except ValueError:
+                pass
+            try:
+                filenames[:] = (fn for fn in filenames if not fn.startswith('.git'))
+            except ValueError:
+                pass
+            try:
+                dirnames.remove('tmp')
+            except ValueError:
+                pass
 
-echo "Tar this package up..."
-cd ${startdir}
-tar zcvf ${name}.tar.gz $workingdir/
-echo "${name}.tar.gz has been generated."
+            for fn in filenames:
+                p = os.path.join(dirpath, fn)
+                h = hashlib.sha1()
+                with file(p, 'rb') as f:
+                    while True:
+                        data = f.read(8*1024*1024)
+                        if not data:
+                            break
+                        h.update(data)
+                sums.write('{h} *{p}\n'.format(
+                        h=h.hexdigest(),
+                        p=p,
+                        ))
+
+
+def rsync(args):
+    """
+    Rsync the barclamp to a Crowbar server.
+    """
+    name = args.yaml['barclamp']['name']
+    dest = args.dest
+    if '@' not in dest:
+        dest = 'crowbar@' + dest
+    subprocess.check_call(
+        args=[
+            'rsync',
+            '-av',
+            '--exclude=.git*',
+            '--exclude=cache/tmp',
+            './',
+            '{dest}:{name}/'.format(
+                dest=dest,
+                name=name,
+                ),
+            ]
+        )
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Package barclamp',
+        )
+
+    sub = parser.add_subparsers(
+        title='commands',
+        metavar='COMMAND',
+        help='description',
+        )
+
+    p = sub.add_parser('download', help=download.__doc__)
+    p.set_defaults(
+        # ugly kludge but i really want to have a nice way to access
+        # the program name, with subcommand, later
+        prog=p.prog,
+        func=download,
+        )
+
+    p = sub.add_parser('sha1', help=rsync.__doc__)
+    p.set_defaults(
+        prog=p.prog,
+        func=sha1,
+        )
+
+    p = sub.add_parser('rsync', help=rsync.__doc__)
+    p.add_argument(
+        'dest',
+        help='user@host to rsync to',
+        )
+    p.set_defaults(
+        prog=p.prog,
+        func=rsync,
+        )
+
+    parser.set_defaults(
+        # we want to hold on to this, for later
+        prog=parser.prog,
+        )
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    try:
+        try:
+            with file('crowbar.yml', 'rb') as f:
+                args.yaml = yaml.safe_load(f)
+        except IOError as e:
+            if e.errno == errno.ENOENT:
+                raise Error('must be run in a directory that stores a barclamp.')
+        return args.func(args)
+    except Error as e:
+        print >>sys.stderr, '{prog}: {msg}'.format(
+            prog=args.prog,
+            msg=e,
+            )
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
+# echo "Downloading Gems..."
+# for gem in ${gemlist}; do
+# 	gem fetch ${gem}
+# done
+# echo "Done."
+
+# echo "Tar this package up..."
+# name=`grep 'name: ' ${workingdir}/crowbar.yml | awk {'print \$2'}`
+# # cd ${startdir}
+# # tar zcvf ${name}.tar.gz $workingdir/
+# # echo "${name}.tar.gz has been generated."
